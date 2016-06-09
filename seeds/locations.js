df324a834a3fc35d942ef528968b464fdae3cbb5
Ian Smith <iansmith1026@gmail.com>
2016-06-09 15:56:04 -0700

    added locations seed

--- seeds/locations.js
+++ seeds/locations.js
@@ -1,12 +1,73 @@
+'use strict';
 
 exports.seed = function(knex, Promise) {
   return Promise.join(
-    // Deletes ALL existing entries
-    knex('table_name').del(),
+    knex('locations').del(),
 
-    // Inserts seed entries
-    knex('table_name').insert({id: 1, colName: 'rowValue'}),
-    knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    knex('table_name').insert({id: 3, colName: 'rowValue3'})
+    knex('locations').insert({
+      id: 1,
+      restaurant_id: 1,
+      street: '123 Somewhere Street',
+      city: 'Seattle',
+      state: 'WA',
+      zipcode: '98109',
+      phone: '206-555-1234',
+      created_at: new Date(),
+      updated_at: new Date()
+    }),
+
+    knex('locations').insert({
+      id: 2,
+      restaurant_id: 1,
+      street: '456 Central Avenue',
+      city: 'Tacoma',
+      state: 'WA',
+      zipcode: '98401',
+      phone: '206-555-5678',
+      created_at: new Date(),
+      updated_at: new Date()
+    }),
+
+    knex('locations').insert({
+      id: 3,
+      restaurant_id: 2,
+      street: '1289 Dublin Way',
+      city: 'Olympia',
+      state: 'WA',
+      zipcode: '98501',
+      phone: '206-555-8329',
+      created_at: new Date(),
+      updated_at: new Date()
+    }),
+
+    knex('locations').insert({
+      id: 4,
+      restaurant_id: 3,
+      street: '1111 Taco Street',
+      city: 'Seattle',
+      state: 'WA',
+      zipcode: '98107',
+      phone: '206-555-0990',
+      created_at: new Date(),
+      updated_at: new Date()
+    })
+
+    // knex('locations').insert({
+    //   id: 2,
+    //   name: '',
+    //   kind: '',
+    //   bio: '',
+    //   dollars: '',
+    //   opened_at: new Date()
+    // }),
+    //
+    // knex('locations').insert({
+    //   id: 3,
+    //   name: '',
+    //   kind: '',
+    //   bio: '',
+    //   dollars: '',
+    //   opened_at: new Date()
+    // })
   );
 };
