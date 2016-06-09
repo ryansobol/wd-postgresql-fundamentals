de94da7579d5d73941df71175bd995c681f9c9bf
Ian Smith <iansmith1026@gmail.com>
2016-06-09 16:45:22 -0700

    added more data to dishes seed file

--- seeds/b_dishes.js
+++ seeds/b_dishes.js
@@ -31,29 +31,37 @@ exports.seed = function(knex, Promise) {
       id: 3,
       restaurant_id: 2,
       name: 'Danny Boy\'s Pad Thai Pie',
-      description: 'Quite a unique dish, this is a fusion of classic pad Thai inside of a pot pie. Yum and gluten-free! (Somehow)',
+      description: 'Quite a unique dish, this is a fusion of classic pad Thai inside of a pot pie. Yummy and gluten-free! (Somehow)',
       cost: 7.75,
       vegetarian_at: null,
       gluten_free_at: new Date(),
       created_at: new Date(),
       updated_at: new Date()
+    }),
+
+    knex('dishes').insert({
+      id: 4,
+      restaurant_id: 3,
+      name: 'The Mosquito Burrito',
+      description: 'A delectable mixture of rice, beans, guac, and cheese. It will definitely get you buzzing!',
+      cost: 9.99,
+      vegetarian_at: null,
+      gluten_free_at: null,
+      created_at: new Date(),
+      updated_at: new Date()
+    }),
+
+    knex('dishes').insert({
+      id: 5,
+      restaurant_id: 3,
+      name: 'Bumblebee Gluten Free Nachos',
+      description: 'A whole heap of gluten-free nachos covered in gluten-free nacho cheese, jalapenos, and beans.',
+      cost: 8.95,
+      vegetarian_at: null,
+      gluten_free_at: new Date(),
+      created_at: new Date(),
+      updated_at: new Date()
     })
-    // knex('restaurants').insert({
-    //   id: 2,
-    //   name: '',
-    //   kind: '',
-    //   bio: '',
-    //   dollars: '',
-    //   opened_at: new Date()
-    // }),
-    //
-    // knex('restaurants').insert({
-    //   id: 3,
-    //   name: '',
-    //   kind: '',
-    //   bio: '',
-    //   dollars: '',
-    //   opened_at: new Date()
-    // })
+
   );
 };
