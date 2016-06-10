c708a06deeaa3abf4d3a6f31485f771f45429db3
Ian Smith <iansmith1026@gmail.com>
2016-06-10 15:08:14 -0700

    minor tweaks to index.js

--- seeds/a_customers.js
+++ seeds/a_customers.js
@@ -8,48 +8,48 @@ module.exports.seed = function(knex, Promise) {
       id: 1,
       name: 'Johnny Walker',
       email: 'drinks@home.com',
-      created_at: new Date(),
-      updated_at: new Date()
+      created_at: new Date(2000, 5, 20),
+      updated_at: new Date(2000, 5, 20)
     }),
 
     knex('customers').insert({
       id: 2,
       name: 'Percy DoLittle',
       email: 'getterdone@gmail.com',
-      created_at: new Date(),
-      updated_at: new Date()
+      created_at: new Date(2000, 5, 20),
+      updated_at: new Date(2000, 5, 20)
     }),
 
     knex('customers').insert({
       id: 3,
       name: 'Rainbow Huff\'n\'Puff',
       email: 'after1234@aol.com',
-      created_at: new Date(),
-      updated_at: new Date()
+      created_at: new Date(2000, 5, 20),
+      updated_at: new Date(2000, 5, 20)
     }),
 
     knex('customers').insert({
       id: 4,
       name: 'Prince',
       email: '@',
-      created_at: new Date(),
-      updated_at: new Date()
+      created_at: new Date(2000, 5, 20),
+      updated_at: new Date(2000, 5, 20)
     }),
 
     knex('customers').insert({
       id: 5,
       name: 'Sue "McGonnigal" Samwortherton',
       email: 'theCharmer@hotmail.com',
-      created_at: new Date(),
-      updated_at: new Date()
+      created_at: new Date(2000, 5, 20),
+      updated_at: new Date(2000, 5, 20)
     }),
 
     knex('customers').insert({
       id: 6,
       name: 'Little baby Tomkins',
       email: 'whaaaaa@ups.com',
-      created_at: new Date(),
-      updated_at: new Date()
+      created_at: new Date(2000, 5, 20),
+      updated_at: new Date(2000, 5, 20)
     })
   );
 };
--- test/index.test.js
+++ test/index.test.js
@@ -47,8 +47,8 @@ suite('index', function() {
     });
   });
 
-  test('find customer names', function() {
-    const actual = index.findCustomerNames(knex);
+  test('find customer names', () => {
+    const actual = index.selectCustomerNames(knex);
     const expected = [
       { name: 'Johnny Walker' },
       { name: 'Percy DoLittle' },
@@ -56,6 +56,128 @@ suite('index', function() {
       { name: 'Prince' },
       { name: 'Sue "McGonnigal" Samwortherton' },
       { name: 'Little baby Tomkins' }
+    ];
+
+    return assert.eventually.sameDeepMembers(actual, expected);
+  });
+
+  test('find restaurants', () => {
+    const actual = index.selectAllRestaurants(knex);
+    const expected = [
+      {
+        id: 1,
+        name: "Hal's Hot Dawg Stand",
+        kind: 'American',
+        bio: 'Hal\'s is the place for all your burgers and dawg needs. We have shakes galore!',
+        dollars: '1',
+        opened_at: new Date(1977, 3, 1),
+        created_at: new Date(2000, 5, 20),
+        updated_at: new Date(2000, 5, 20)
+      },
+      {
+        id: 2,
+        name: 'McDouglas\'s Irish Fusion Cafe',
+        kind: 'Irish/Thai',
+        bio: 'You can\'t believe what this amazing Thai Irish pub fusion creates. Trust us, you\'ll love it.',
+        dollars: '2',
+        opened_at: new Date(2015,11,31),
+        created_at: new Date(2000, 5, 20),
+        updated_at: new Date(2000, 5, 20)
+      },
+      {
+        id: 3,
+        name: 'Joe\'s Burritos',
+        kind: 'Mexican',
+        bio: 'Not the best burrito in town, if you want better, try Jose\'s.',
+        dollars: '1',
+        opened_at: new Date(1997, 7, 22),
+        created_at: new Date(2000, 5, 20),
+        updated_at: new Date(2000, 5, 20)
+      },
+      {
+        id: 4,
+        name: 'Chan\'s China Palace',
+        kind: 'Chinese',
+        bio: 'A hidden gem in the Pacific Northwest. Upscale dining with an incredible menu and great wait staff.',
+        dollars: '4',
+        opened_at: new Date(1991, 3, 8),
+        created_at: new Date(2000, 5, 20),
+        updated_at: new Date(2000, 5, 20)
+      },
+      {
+        id: 5,
+        name: 'Benny\'s Meatballs',
+        kind: 'Italian',
+        bio: 'The best meatballs in town! Affordable and tasty, these meatballs will have you coming back for more.',
+        dollars: '3',
+        opened_at: new Date(2004, 10, 17),
+        created_at: new Date(2000, 5, 20),
+        updated_at: new Date(2000, 5, 20)
+      }
+    ];
+
+    return assert.eventually.sameDeepMembers(actual, expected);
+  });
+
+  test('select unique customer', () => {
+    const actual = index.selectCustomerById(knex);
+    const expected = [
+      {
+        id: 1,
+        name: 'Johnny Walker',
+        email: 'drinks@home.com',
+        created_at: new Date(2000, 5, 20),
+        updated_at: new Date(2000, 5, 20)
+      }
+    ];
+
+    return assert.eventually.deepEqual(actual, expected);
+  });
+
+  test('select unique city', () => {
+    const actual = index.selectCityById(knex);
+    const expected = [
+      {
+        id: 3,
+        restaurant_id: 2,
+        street: '1289 Dublin Way',
+        city: 'Olympia',
+        state: 'WA',
+        zipcode: '98501',
+        phone: '206-555-8329',
+        created_at: new Date(2000, 5, 20),
+        updated_at: new Date(2000, 5, 20)
+      }
+    ];
+
+    return assert.eventually.deepEqual(actual, expected);
+  });
+
+  test('select number of distinct cities', () => {
+    const actual = index.selectNumberOfDistinctCities(knex);
+    const expected = [
+      {
+        count: '3'
+      }
+    ];
+
+    return assert.eventually.deepEqual(actual, expected);
+  });
+
+  test('select cheapest dish', () => {
+    const actual = index.selectCheapestDish(knex);
+    const expected = [
+      {
+        id: 1,
+        restaurant_id: 1,
+        name: 'the killer chili dawg',
+        description: 'The ULTIMATE test.',
+        cost: 7.65,
+        vegetarian_at: null,
+        gluten_free_at: null,
+        created_at: new Date(2000, 5, 20),
+        updated_at: new Date(2000, 5, 20)
+      }
     ];
 
     return assert.eventually.sameDeepMembers(actual, expected);
