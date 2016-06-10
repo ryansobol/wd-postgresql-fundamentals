0ec80075742711bcfebf048eaa3e22bef4fb5c99
Ian Smith <iansmith1026@gmail.com>
2016-06-10 13:43:15 -0700

    added more seed data to all tables

--- seeds/a_restaurants.js
+++ seeds/a_restaurants.js
@@ -35,6 +35,28 @@ module.exports.seed = function(knex, Promise) {
        opened_at: new Date(1997, 7, 22),
        created_at: new Date(),
        updated_at: new Date()
+     }),
+
+     knex('restaurants').insert({
+       id: 4,
+       name: 'Chan\'s China Palace',
+       kind: 'Chinese',
+       bio: 'A hidden gem in the Pacific Northwest. Upscale dining with an incredible menu and great wait staff.',
+       dollars: '4',
+       opened_at: new Date(1991, 3, 8),
+       created_at: new Date(),
+       updated_at: new Date()
+     }),
+
+     knex('restaurants').insert({
+       id: 5,
+       name: 'Benny\'s Meatballs',
+       kind: 'Italian',
+       bio: 'The best meatballs in town! Affordable and tasty, these meatballs will have you coming back for more.',
+       dollars: '3',
+       opened_at: new Date(2004, 10, 17),
+       created_at: new Date(),
+       updated_at: new Date()
      })
   );
 };
