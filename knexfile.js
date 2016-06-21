a6a331010375a9801361d19eddfa63082856ddc2
Ryan Sobol <contact@ryansobol.com>
2016-06-21 01:38:06 -0700

    Move migrations and seeds into part4 directory

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,12 @@
 module.exports = {
   part4: {
     client: 'pg',
-    connection: 'postgres://localhost/yowl_part4'
+    connection: 'postgres://localhost/yowl_part4',
+    migrations: {
+      directory: './migrations/part4'
+    },
+    seeds: {
+      directory: './seeds/part4'
+    }
   }
 };
